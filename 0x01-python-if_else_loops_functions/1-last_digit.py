251a1633eac39ca44fcbda48cd043ec7655755e4
sylviajebet <sylviajebett@gmail.com>
2022-06-28 07:27:16 -0700

    Python script to read the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+# find last digit for ngatives and positives
+if number < 0:
+    lastDgt = ((-1*number) % 10)
+else:
+    lastDgt = number % 10
+
+# output the last digit of the number
+if lastDgt == 0:
+    print(f"Last digit of {number:d} is {lastDgt:d} and is 0")
+elif lastDgt > 5:
+    print(f"Last digit of {number:d} is {lastDgt:d} and is greater\
+ than 5")
+elif lastDgt < 6 and lastDgt != 0:
+    print(f"Last digit of {number:d} is {lastDgt:d} and is\
+ less than 6 and not 0")
