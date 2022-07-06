0724c06abe3d3c32409b96e758cf1e1ecfc39f7e
sylviajebet <sylviajebett@gmail.com>
2022-07-06 08:59:32 -0700

    Function that replaces all occurences of an element by another

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#Function to replace all occurences of an element with another
+def search_replace(my_list, search, replace):
+	rep_list = []
+	for i in my_list:
+		if i == search:
+			rep_list.append(replace)
+		else:
+			rep_list.append(i)
+	return rep_list
