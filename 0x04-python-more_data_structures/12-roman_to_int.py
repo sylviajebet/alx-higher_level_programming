0b5dbd315f9be6ad7eee53b0094f389b76430b13
sylviajebet <sylviajebett@gmail.com>
2022-07-06 09:45:09 -0700

    File corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+
+# roman_to_int - Function to convert roman numbers to integers
 def roman_to_int(roman_string):
     roman_num = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
 	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
