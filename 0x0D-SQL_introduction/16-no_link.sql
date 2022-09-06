5536261b16340712253e4c74711f3eec6dbed0b0
sylviajebet <sylviajebett@gmail.com>
2022-09-07 00:55:08 +0300

    File corrected

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of second_table 
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY name DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
